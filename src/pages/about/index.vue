d5910bb3467a4a13b38c230831fd0e41d74c15ca
Ken <3408238124@qq.com>
2023-05-19 16:12:09 +0800

    save

---
+++ src/pages/about/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <view class="content">
+        <image class="logo" src="/static/logo.png" />
+        <view class="text-area">
+            <text class="title">{{ title }}</text>
+            <button @click="toAbout">去首页</button>
+        </view>
+    </view>
+</template>
+
+<script setup lang="ts">
+import { ref, getCurrentInstance } from 'vue'
+const title = ref('去首页')
+const toAbout = () => {
+    uni.navigateTo({
+        url: '/pages/index/index'
+    })
+}
+onLoad(() => {
+    // 通讯获取事件
+    const instance = getCurrentInstance()?.proxy
+    // @ts-expect-error-error
+    const eventChannel = instance.getOpenerEventChannel()
+})
+</script>
+
+<style>
+.content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin-top: 200rpx;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50rpx;
+}
+
+.text-area {
+    display: flex;
+    justify-content: center;
+}
+
+.title {
+    font-size: 36rpx;
+    color: #8f8f94;
+}
+</style>
